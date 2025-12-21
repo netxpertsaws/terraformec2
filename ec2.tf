4b59fc8e4745404327abbb4e4b3350869c971125
netxpertsaws <98447003+netxpertsaws@users.noreply.github.com>
2025-12-21 18:59:12 +0530

    Add Terraform configuration for AWS provider

--- ec2.tf
+++ ec2.tf
@@ -1,3 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.27.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+
+
 resource "aws_instance" "this" {
   count                   = "1"
   ami                     = "ami-0e86e20dae9224db8"
